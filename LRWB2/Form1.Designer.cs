f1a596a4a70f59a50ea75a667b60e796695fc8c7
andri <andri@XIAOMIMIBOOK>
2023-11-26 15:03:12 +0200

    Add project files.

---
+++ LRWB2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace LRWB2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.addressBar = new System.Windows.Forms.TextBox();
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.goButton = new System.Windows.Forms.Button();
+            this.AddTabButton = new System.Windows.Forms.Button();
+            this.RamLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addressBar
+            // 
+            this.addressBar.Location = new System.Drawing.Point(0, 12);
+            this.addressBar.Name = "addressBar";
+            this.addressBar.Size = new System.Drawing.Size(666, 20);
+            this.addressBar.TabIndex = 0;
+            this.addressBar.Text = "https://www.google.com/";
+            this.addressBar.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.BackColor = System.Drawing.SystemColors.Control;
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(0, 38);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(799, 412);
+            this.webView.Source = new System.Uri("https://www.google.com/", System.UriKind.Absolute);
+            this.webView.TabIndex = 1;
+            this.webView.ZoomFactor = 1D;
+            this.webView.NavigationCompleted += new System.EventHandler<Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs>(this.CoreWebView2_NavigationCompleted);
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(749, 10);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(50, 23);
+            this.goButton.TabIndex = 2;
+            this.goButton.Text = "GO";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // AddTabButton
+            // 
+            this.AddTabButton.Location = new System.Drawing.Point(672, 10);
+            this.AddTabButton.Name = "AddTabButton";
+            this.AddTabButton.Size = new System.Drawing.Size(71, 23);
+            this.AddTabButton.TabIndex = 3;
+            this.AddTabButton.Text = "Add Tab";
+            this.AddTabButton.UseVisualStyleBackColor = true;
+            this.AddTabButton.Click += new System.EventHandler(this.AddTabButton_Click);
+            // 
+            // RamLabel
+            // 
+            this.RamLabel.AutoSize = true;
+            this.RamLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.RamLabel.Location = new System.Drawing.Point(538, 12);
+            this.RamLabel.Name = "RamLabel";
+            this.RamLabel.Size = new System.Drawing.Size(107, 20);
+            this.RamLabel.TabIndex = 4;
+            this.RamLabel.Text = "RAM USAGE";
+            this.RamLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.RamLabel);
+            this.Controls.Add(this.AddTabButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.addressBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Low Ram Web Viewer";
+            this.Load += new System.EventHandler(this.Form_Resize);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
+            this.SystemColorsChanged += new System.EventHandler(this.Form1_SystemColorsChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox addressBar;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button AddTabButton;
+        private System.Windows.Forms.Label RamLabel;
+    }
+}
+
---
+++ LRWB2/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Microsoft.Web.WebView2.Core;
+
+namespace LRWB2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.Form_Resize);
+            if (webView != null && webView.CoreWebView2 != null && addressBar.Text != webView.CoreWebView2.Source) { addressBar.Text = webView.CoreWebView2.Source; }
+        }
+
+        private void CoreWebView2_NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            addressBar.Text = webView.CoreWebView2.Source;
+        }
+
+
+        private void Form_Resize(object sender, EventArgs e)
+        {
+            webView.Size = this.ClientSize - new System.Drawing.Size(webView.Location);
+            goButton.Left = this.ClientSize.Width - goButton.Width;
+            AddTabButton.Left = this.ClientSize.Width - AddTabButton.Width - 55;
+            addressBar.Width = goButton.Left - addressBar.Left- 100;
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            goButton_Click(sender, e);
+        }
+
+        public void goButton_Click(object sender, EventArgs e)
+        {
+            if (webView != null && webView.CoreWebView2 != null)
+            {
+                if (addressBar.Text.Contains("https:")) { webView.CoreWebView2.Navigate(addressBar.Text); }
+                else { webView.CoreWebView2.Navigate("https://" + addressBar.Text); }
+                addressBar.Text = webView.CoreWebView2.Source;
+            }
+        }
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                goButton_Click(sender, e);
+        }
+
+        private void AddTabButton_Click(object sender, EventArgs e)
+        {
+            Form1 nTab = new Form1();
+            nTab.Show();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Process c_p = System.Diagnostics.Process.GetCurrentProcess();
+            float m_u = c_p.WorkingSet64;
+            var mbUsed = (c_p.PrivateMemorySize64 / 1024) / 1024;
+            RamLabel.Text = (m_u / 1000000).ToString();
+        }
+
+        private void Form1_SystemColorsChanged(object sender, EventArgs e)
+        {
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+        }
+    }
+}
